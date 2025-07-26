06ef998e812ec169253ba664ed26fb106ee36c69
= <manni.basi@gmail.com>
2025-07-26 16:34:33 +0100

    Refactored to use case statement

--- src/main/java/WcTool.java
+++ src/main/java/WcTool.java
@@ -6,16 +6,20 @@ public class WcTool {
     public String execute(String[] args) throws IOException {
         InputStream inputStream = getClass().getResourceAsStream(args[1]);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        if(args[0].equals("-c")) {
-            return inputStream.readAllBytes().length + " " + args[1];
-        } else if (args[0].equals("-l")) {
-            return reader.lines().count() + " " + args[1];
-        } else if (args[0].equals("-w")) {
-            long wordCount = reader.lines()
-                    .flatMap(line -> Arrays.stream(line.trim().split("\\s")))
-                    .filter(word -> !word.isEmpty())
-                    .count();
-            return wordCount + " " + args[1];
+        switch (args[0]) {
+            case "-c" -> {
+                return inputStream.readAllBytes().length + " " + args[1];
+            }
+            case "-l" -> {
+                return reader.lines().count() + " " + args[1];
+            }
+            case "-w" -> {
+                long wordCount = reader.lines()
+                        .flatMap(line -> Arrays.stream(line.trim().split("\\s")))
+                        .filter(word -> !word.isEmpty())
+                        .count();
+                return wordCount + " " + args[1];
+            }
         }
         throw new IllegalArgumentException("Unknown option: " + args[0]);
     }
