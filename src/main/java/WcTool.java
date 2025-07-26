853d230dfc1a098cd6021c72a9bd1529dcf5d8fa
= <manni.basi@gmail.com>
2025-07-26 16:33:35 +0100

    Added word count implementation

--- src/main/java/WcTool.java
+++ src/main/java/WcTool.java
@@ -1,14 +1,21 @@
 import java.io.*;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 public class WcTool {
     public String execute(String[] args) throws IOException {
         InputStream inputStream = getClass().getResourceAsStream(args[1]);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         if(args[0].equals("-c")) {
             return inputStream.readAllBytes().length + " " + args[1];
         } else if (args[0].equals("-l")) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             return reader.lines().count() + " " + args[1];
+        } else if (args[0].equals("-w")) {
+            long wordCount = reader.lines()
+                    .flatMap(line -> Arrays.stream(line.trim().split("\\s")))
+                    .filter(word -> !word.isEmpty())
+                    .count();
+            return wordCount + " " + args[1];
         }
         throw new IllegalArgumentException("Unknown option: " + args[0]);
     }
