6f6477bd506c086ec1b17d5f687dd7706d27cd9a
= <manni.basi@gmail.com>
2025-07-26 11:11:14 +0100

    Added implementation to return bytes #

--- src/test/java/WcToolTests.java
+++ src/test/java/WcToolTests.java
@@ -1,15 +1,18 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WcToolTests {
     @Test
-    void testByteCount() {
+    void testByteCount() throws IOException {
         WcTool wcTool = new WcTool();
         String filePath = "test.txt";
         String[] args = {"-c", filePath};
 
         String result = wcTool.execute(args);
 
-        assertEquals("  342190 test.txt", result);}
+        assertEquals("342190 test.txt", result);
+    }
 }
